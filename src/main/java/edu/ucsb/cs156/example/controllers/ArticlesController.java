b4db06282ad5a71a601f1c3d09a2d8ea9695d013
Suhrit Padakanti <suhrit@ucsb.edu>
2025-04-28 12:47:15 -0700

    Articles-controller

---
+++ src/main/java/edu/ucsb/cs156/example/controllers/ArticlesController.java
@@ -0,0 +1,88 @@
+package edu.ucsb.cs156.example.controllers;
+
+import edu.ucsb.cs156.example.entities.Article;
+import edu.ucsb.cs156.example.errors.EntityNotFoundException;
+import edu.ucsb.cs156.example.repositories.ArticlesRepository;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.extern.slf4j.Slf4j;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
+
+import java.time.LocalDateTime;
+
+/**
+ * This is a REST controller for Articles
+ */
+
+@Tag(name = "Articles")
+@RequestMapping("/api/articles")
+@RestController
+@Slf4j
+public class ArticlesController extends ApiController {
+
+    @Autowired
+    ArticlesRepository articlesRepository;
+
+    /**
+     * List all articles
+     * 
+     * @return an iterable of Article
+     */
+    @Operation(summary = "List all articles")
+    @PreAuthorize("hasRole('ROLE_USER')")
+    @GetMapping("/all")
+    public Iterable<Article> allArticles() {
+        Iterable<Article> articles = articlesRepository.findAll();
+        return articles;
+    }
+
+    /**
+     * Create a new article
+     * 
+     * @param title         the title of the article
+     * @param url           the url of the article
+     * @param explanation   the explanation of the article
+     * @param email         the email of the submitter
+     * @param dateAdded     the date the article was added
+     * @return the saved article
+     */
+    @Operation(summary = "Create a new article")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @PostMapping("/post")
+    public Article postArticle(
+            @Parameter(name = "title") @RequestParam String title,
+            @Parameter(name = "url") @RequestParam String url,
+            @Parameter(name = "explanation") @RequestParam String explanation,
+            @Parameter(name = "email") @RequestParam String email,
+            @Parameter(name = "dateAdded", description = "date (in iso format, e.g. YYYY-mm-ddTHH:MM:SS)") 
+            @RequestParam("dateAdded") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime dateAdded)
+            throws JsonProcessingException {
+
+        log.info("dateAdded={}", dateAdded);
+
+        Article article = new Article();
+        article.setTitle(title);
+        article.setUrl(url);
+        article.setExplanation(explanation);
+        article.setEmail(email);
+        article.setDateAdded(dateAdded);
+
+        Article savedArticle = articlesRepository.save(article); // <-- fixed here
+
+        return savedArticle;
+    }
+}
