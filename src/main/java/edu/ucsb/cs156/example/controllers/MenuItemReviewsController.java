653ec61483a3cbc4653400bf71f278c41e87bafd
applechristian <christianjlee@ucsb.edu>
2025-04-29 13:13:58 -0700

    cl - add delete endpoint to reviews controller; tests will be in the next commit

--- src/main/java/edu/ucsb/cs156/example/controllers/MenuItemReviewsController.java
+++ src/main/java/edu/ucsb/cs156/example/controllers/MenuItemReviewsController.java
@@ -132,4 +132,22 @@ public class MenuItemReviewsController extends ApiController {
 
         return menuItemReview;
     }
+
+    /**
+     * Delete a Review
+     * 
+     * @param id the id of the review to delete
+     * @return a message indicating the review was deleted
+     */
+    @Operation(summary= "Delete a MenuItemReview")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @DeleteMapping("")
+    public Object deleteMenuItemReview(
+            @Parameter(name="id") @RequestParam Long id) {
+        MenuItemReview menuItemReview = menuItemReviewRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException(MenuItemReview.class, id));
+
+        menuItemReviewRepository.delete(menuItemReview);
+        return genericMessage("MenuItemReview with id %s deleted".formatted(id));
+    }
 }
